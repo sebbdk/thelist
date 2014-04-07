166626bcfaae1db144760ef2ab96639410f2729c
Kasper jensen <sebb@kaspers-mbp.home>
2014-04-07 02:58:28 +0200

    revamped the layout

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* 
+* @Author: sebb
+* @Date:   2014-04-04 20:44:35
+* @Last Modified by:   sebb
+* @Last Modified time: 2014-04-07 02:56:30
+*/
+
+(function($) {
+
+	$(window).on('resize', onResize);
+
+	$(document).ready(function() {
+		onResize();
+		setTimeout(function() {
+			$('body').addClass('active');
+		}, 300);
+
+		loadGames();
+	});
+
+	function loadGames() {
+		$.get('games.json', function(items) {
+			$('small').prepend(items.length + ' ')
+			$.each(items, function(index, item) {
+				var template = $($('#game-template').html());
+
+				template.find('h3').html(item.title);
+				template.find('p').html(item.description);
+
+				if(item.link) {
+					template.find('a').attr('href', item.link);
+				} else {
+					template.find('a').remove();
+				}
+
+				$('body').append(template);
+			});
+		});
+	}
+
+	function onResize() {
+		console.log('resize!');
+		var pos = ($(window).height()/2) - ($('.bubble').outerHeight()/2);
+		$('.bubble').css('margin-top', pos)
+	}
+
+})(jQuery);
